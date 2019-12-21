859d2da6492342154d0520a1b075d098640cbe20
gimbas <silvagracarafael@gmail.com>
2019-12-21 03:16:02 +0000

    make it compliant with r3fuel

---
+++ include/lvgl.h
@@ -0,0 +1,97 @@
+/**
+ * @file lvgl.h
+ * Include all LittleV GL related headers
+ */
+
+#ifndef LVGL_H
+#define LVGL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "lv_version.h"
+
+#include "lv_log.h"
+#include "lv_task.h"
+#include "lv_math.h"
+#include "lv_async.h"
+
+#include "lv_hal.h"
+
+#include "lv_obj.h"
+#include "lv_group.h"
+#include "lv_indev.h"
+
+#include "lv_refr.h"
+#include "lv_disp.h"
+#include "lv_debug.h"
+
+#include "lv_theme.h"
+
+#include "lv_font.h"
+#include "lv_font_fmt_txt.h"
+#include "lv_bidi.h"
+#include "lv_printf.h"
+
+#include "lv_btn.h"
+#include "lv_imgbtn.h"
+#include "lv_img.h"
+#include "lv_label.h"
+#include "lv_line.h"
+#include "lv_page.h"
+#include "lv_cont.h"
+#include "lv_list.h"
+#include "lv_chart.h"
+#include "lv_table.h"
+#include "lv_cb.h"
+#include "lv_cpicker.h"
+#include "lv_bar.h"
+#include "lv_slider.h"
+#include "lv_led.h"
+#include "lv_btnm.h"
+#include "lv_kb.h"
+#include "lv_ddlist.h"
+#include "lv_roller.h"
+#include "lv_ta.h"
+#include "lv_canvas.h"
+#include "lv_win.h"
+#include "lv_tabview.h"
+#include "lv_tileview.h"
+#include "lv_mbox.h"
+#include "lv_gauge.h"
+#include "lv_lmeter.h"
+#include "lv_sw.h"
+#include "lv_kb.h"
+#include "lv_arc.h"
+#include "lv_preload.h"
+#include "lv_calendar.h"
+#include "lv_spinbox.h"
+
+#include "lv_img_cache.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*LVGL_H*/
